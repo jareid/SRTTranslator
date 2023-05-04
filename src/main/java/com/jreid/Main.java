db22054fc444bbea40fd7f32a0801bcb3f382caa
Jamie Reid <jamie.reid@gmx.com>
2023-05-04 13:08:01 +0200

    Update Main (untested)

--- src/main/java/com/jreid/Main.java
+++ src/main/java/com/jreid/Main.java
@@ -3,6 +3,7 @@ package com.jreid;
 import com.jreid.srt.SRTParser;
 import com.jreid.srt.SRTTranslator;
 import com.jreid.srt.Subtitle;
+import com.jreid.utils.LanguageCodes;
 import com.jreid.utils.ResourceUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -13,9 +14,12 @@ public class Main {
     public static void main(String[] args) {
         LOGGER.info("Application started");
         if (args.length == 3) {
-            Subtitle testSubtitle = SRTParser.getSubtitlesFromFile(args[0], false, false);
-            String outputName = args[0].substring(0,args[0].length() - 4) + "_" + args[2] + ".srt";
-            Subtitle translatedTestSubtitle = SRTTranslator.translate( ResourceUtil.getGoogleApiKey(), testSubtitle, args[1], args[2]);
+            String fileName = "files/sub.srt";
+            String outputName = fileName.substring(0,fileName.length() - 4) + "_" + LanguageCodes.SPANISH + ".srt";
+
+            Subtitle testSubtitle = SRTParser.getSubtitlesFromFile(fileName, false, false);
+            Subtitle translatedTestSubtitle = SRTTranslator.translate( ResourceUtil.getGoogleApiKey(), testSubtitle,
+                                                                       LanguageCodes.ENGLISH, LanguageCodes.SPANISH);
             Subtitle.toFile(outputName, translatedTestSubtitle);
         } else {
             LOGGER.error("Missing command line arguments, should be \"text languageFrom languageTo\"");
